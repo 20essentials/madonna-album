c82f092af870bd80ff2e3e31e9ae0f5407020596
DominicDev <s0l0estudi4r@gmail.com>
2025-12-27 10:16:36 +0100

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'MADONNA';
+export const nameSongs = [
+	"LIKE A PRAYER", /*1*/
+	"VOGUE", /*2*/
+	"LIKE A VIRGIN", /*3*/
+	"MATERIAL GIRL", /*4*/
+	"HOLIDAY", /*5*/
+	"INTO THE GROOVE", /*6*/
+	"LA ISLA BONITA", /*7*/
+	"PAPA DON'T PREACH", /*8*/
+	"FROZEN", /*9*/
+	"HUNG UP", /*10*/
+	"RAY OF LIGHT", /*11*/
+	"OPEN YOUR HEART", /*12*/
+	"EXPRESS YOURSELF", /*13*/
+	"LIVE TO TELL", /*14*/
+	"BORDERLINE", /*15*/
+	"LUCKY STAR", /*16*/
+	"DON'T TELL ME", /*17*/
+	"SECRET", /*18*/
+	"JUSTIFY MY LOVE", /*19*/
+	"DRESS YOU UP", /*20*/
+]
